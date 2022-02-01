c4d11aedd22eb7de6b4a5226327fa6e93b034b56
Felipe Mellado U <famellad@gmail.com>
2022-02-01 16:12:39 -0300

    Add minimal canvas code

---
+++ main.js
@@ -0,0 +1,18 @@
+var canvasElement = document.getElementById("canvas");
+console.log(document.getElementById("canvas"));
+var ctx = canvasElement.getContext("2d");
+
+ctx.moveTo( 100, 100);
+
+ctx.font = "188px sans";
+
+ctx.fillText("SILK", 200, 200);
+
+// Caja 1
+// wh:605, 303 xy:40, 40
+
+// Caja 2
+// wh: 454, 303 xy: 40, 380
+
+// Caja 3
+// wh: 114, 1010 xy: 680, 20
